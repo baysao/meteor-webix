24c3c0712107f6723b7362530d82ece8365b61e3
Vu Tran <baysao@gmail.com>
2015-10-29 20:27:43 +0700

    change files dependences

--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
   name: 'baysao:meteor-webix',
   version: '0.1.0-6',
-  // Brief, one-line summary of the package.
   summary: 'Meteor Webix',
-  // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/baysao/meteor-webix.git',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
@@ -17,7 +13,6 @@ Package.onUse(function(api) {
     './codebase/webix.js',
     './codebase/webix.css'
   ], 'client');
-  //api.addFiles(['./lib/core.js', './lib/menu.js'], 'client');
   api.addAssets([
     './codebase/fonts/fontawesome-webfont.svg',
     './codebase/fonts/fontawesome-webfont.ttf',
@@ -31,8 +26,5 @@ Package.onUse(function(api) {
     './codebase/fonts/PTS-bold.ttf'
     ], 'client');
   api.export('webix', 'client');
-  //api.export('WebixRouter', 'client');
-  //api.export('WebixMenuHelper', 'client');
-  //api.export('WebixViews', 'client');
 });
 
